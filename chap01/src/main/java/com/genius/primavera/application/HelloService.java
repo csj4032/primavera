79ef97f0856eb169931f09d4ed02e85ed508c4da
genius.choi <csj4032@gmail.com>
2019-04-09 00:44:20 +0900

    chap01

---
+++ chap01/src/main/java/com/genius/primavera/application/HelloService.java
@@ -0,0 +1,8 @@
+package com.genius.primavera.application;
+
+import java.util.List;
+
+public interface HelloService {
+
+	List<String> getArticles();
+}
---
+++ chap01/src/test/java/com/genius/primavera/interfaces/HelloControllerTest.java
@@ -0,0 +1,46 @@
+package com.genius.primavera.interfaces;
+
+import com.genius.primavera.application.HelloService;
+import org.hamcrest.core.IsAnything;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private HelloService helloService;
+
+	@Test
+	public void indexPage() throws Exception {
+		mockMvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk())
+				.andExpect(content().string(IsAnything.anything("hello world")));
+	}
+
+	@Test
+	public void indexPageArticle() throws Exception {
+		given(this.helloService.getArticles()).willReturn(List.of("게시글1", "게시글2"));
+		mockMvc.perform(get("/articles").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andExpect(content().string(("[\"게시글1\",\"게시글2\"]")));
+	}
+}
