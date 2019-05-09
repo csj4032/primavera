23221d5ad55ae98de3c4ad948ad7dc9888668feb
genius.choi <csj4032@gmail.com>
2019-05-09 16:39:25 +0900

    chap08

--- chap08/src/main/java/com/genius/primavera/infrastructure/security/PrimaveraSecurityConfig.java
+++ chap08/src/main/java/com/genius/primavera/infrastructure/security/PrimaveraSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.genius.primavera.infrastructure.security;
 
-import com.genius.primavera.application.UserService;
-import com.genius.primavera.application.UserServiceImpl;
 import com.genius.primavera.infrastructure.filter.PrimaveraFilter;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
