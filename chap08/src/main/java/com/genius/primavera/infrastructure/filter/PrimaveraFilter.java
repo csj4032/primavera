d00e07217120c9416d69971ff018be818239c6e8
genius.choi <csj4032@gmail.com>
2019-05-09 12:29:31 +0900

    Spring Security

--- chap08/src/main/java/com/genius/primavera/infrastructure/filter/PrimaveraFilter.java
+++ chap08/src/main/java/com/genius/primavera/infrastructure/filter/PrimaveraFilter.java
@@ -1,6 +1,7 @@
 package com.genius.primavera.infrastructure.filter;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
@@ -21,20 +23,9 @@ import static org.springframework.security.web.context.HttpSessionSecurityContex
 @Component
 public class PrimaveraFilter extends OncePerRequestFilter {
 
-	private AntPathMatcher antPathMatcher = new AntPathMatcher();
-
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-		if (antPathMatcher.match("/login", request.getRequestURI())) {
-			response.setHeader("primavera", "filter");
-			if(request.getMethod().equals("POST")) {
-				var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(request.getParameter("email"), request.getParameter("password"));
-				SecurityContext securityContext =  SecurityContextHolder.getContext();
-				securityContext.setAuthentication(usernamePasswordAuthenticationToken);
-				HttpSession session = request.getSession(true);
-				session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
-			}
-		}
-		filterChain.doFilter(request, response);
-	}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        log.info("PrimaveraFilter");
+        filterChain.doFilter(request, response);
+    }
 }
--- chap08/src/main/java/com/genius/primavera/infrastructure/security/SecurityConfig.java
+++ chap08/src/main/java/com/genius/primavera/infrastructure/security/SecurityConfig.java
@@ -1,8 +1,56 @@
 package com.genius.primavera.infrastructure.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("Genius").password("{bcrypt}$2a$10$7UEHLpn1r4gZY2qxiZFJ5.7wa3Hdz8IXgxUtFogy0Ac10fh7TG4V.").roles("USER")
+                .and()
+                .withUser("Marcus Tullius Cicero").password(encoder.encode("password")).roles("MANAGER")
+                .and()
+                .withUser("Julius Caesar").password(encoder.encode("password")).roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers(HttpMethod.GET, "/resources/**", "/bower_components/**", "/dist/**", "/plugins/**", "/favicon.ico");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/index").hasAnyRole("USER", "MANAGER", "ADMIN")
+                .antMatchers("/manager").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .loginPage("/login")
+                .loginProcessingUrl("/signin")
+                .defaultSuccessUrl("/index", true)
+                .failureUrl("/login?error=true")
+                .and()
+                .logout()
+                .logoutUrl("/signout")
+                .deleteCookies("JSESSIONID");
+    }
 }
