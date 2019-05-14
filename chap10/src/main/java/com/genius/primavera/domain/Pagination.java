0bdd5a00e2ec27dc0c1df98056fa3d67fb932dff
genius.choi <csj4032@gmail.com>
2019-05-15 01:06:56 +0900

    chap10

--- chap10/src/main/java/com/genius/primavera/domain/Pagination.java
+++ chap10/src/main/java/com/genius/primavera/domain/Pagination.java
@@ -10,15 +10,42 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Pagination<T> {
-    private int pageNumber;
-    private int pageSize = 10;
-    private int totalElements;
-    private int totalPages = 10;
-    private List<T> contents;
+	private int pageNumber = 1;
+	private int pageSize = 10;
+	private int totalElements;
+	private int totalPages;
+	private int totalSize = 10;
+	private int pageTotal = 1;
+	private List<T> contents;
 
-    public List<Integer> getPageNumber() {
-        int firstPage = ((pageNumber * pageSize) / pageSize) + 1;
-        int lastPage = firstPage + totalPages;
-        return IntStream.rangeClosed(firstPage, lastPage).boxed().collect(Collectors.toList());
-    }
+	public List<Integer> getPagesNumber() {
+		int firstPage = (getPageByTotalSize() * totalSize) + 1;
+		int lastPage = Math.min((firstPage + totalSize - 1), getTotalPages());
+		return IntStream.rangeClosed(firstPage, lastPage).boxed().collect(Collectors.toList());
+	}
+
+	public int getPageByTotalSize() {
+		int pageByTotalSize = (int) Math.ceil((double) pageNumber / (double) totalSize);
+		return pageByTotalSize == 1 ? 0 : pageByTotalSize - 1;
+	}
+
+	public int getTotalPages() {
+		return getTotalElements() == 0 ? 1 : (int) Math.ceil((double) totalElements / (double) pageSize);
+	}
+
+	public boolean isFirst() {
+		return !hasPrevious();
+	}
+
+	public boolean isLast() {
+		return !hasNext();
+	}
+
+	public boolean hasPrevious() {
+		return getPageNumber() > 1;
+	}
+
+	public boolean hasNext() {
+		return getPageNumber() < getTotalPages();
+	}
 }
