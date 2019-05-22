b1659c3d07869ffbb2c9cf42c64ce7256db47327
genius.choi <csj4032@gmail.com>
2019-05-23 00:59:48 +0900

    enum

---
+++ chap11/src/test/java/com/genius/primavera/domain/DeviceTest.java
@@ -0,0 +1,13 @@
+package com.genius.primavera.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class DeviceTest {
+
+	public static void main(String[] args) {
+
+	}
+}
