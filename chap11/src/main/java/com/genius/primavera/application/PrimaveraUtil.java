cfb5ec74bfdd39d7c85d38fcb163203c4a6bfb9b
genius.choi <csj4032@gmail.com>
2019-05-30 14:43:17 +0900

    chap11

--- chap11/src/main/java/com/genius/primavera/application/PrimaveraUtil.java
+++ chap11/src/main/java/com/genius/primavera/application/PrimaveraUtil.java
@@ -4,6 +4,10 @@ import com.genius.primavera.domain.model.user.User;
 import com.genius.primavera.infrastructure.security.PrimaveraUserDetails;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import lombok.experimental.UtilityClass;
 
@@ -13,5 +17,30 @@ public class PrimaveraUtil {
     public static User getUser() {
         PrimaveraUserDetails primaveraUserDetails = (PrimaveraUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return primaveraUserDetails.getUser();
+    }
+
+    public static <T> List<T> toList( final Iterable<T> iterable) {
+        if (iterable instanceof List) {
+            return (List<T>) iterable;
+        } else {
+            List<T> list = new ArrayList<>();
+            for (T item : iterable) {
+                list.add(item);
+            }
+            return list;
+        }
+    }
+
+    public static <T> Set<T> toSet( Iterable<T> source) {
+        if (source instanceof Set) {
+            return (Set<T>) source;
+        } else {
+            Set<T> set = new HashSet<>();
+
+            for (T item : source) {
+                set.add(item);
+            }
+            return set;
+        }
     }
 }
