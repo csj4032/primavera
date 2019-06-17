342f3fc1c08d2d7c8fae900c5f5ddc3cdfdce350
genius.choi <genius.choi@wemakeprice.com>
2019-06-17 14:48:37 +0900

    chap01

--- chap01/src/main/java/com/genius/primavera/application/HelloService.java
+++ chap01/src/main/java/com/genius/primavera/application/HelloService.java
@@ -3,8 +3,9 @@ package com.genius.primavera.application;
 import java.util.List;
 
 public interface HelloService {
-	String getArticle(int i);
 
-	List<String> getArticles();
+    String getArticle(int i);
+
+    List<String> getArticles();
 
 }
