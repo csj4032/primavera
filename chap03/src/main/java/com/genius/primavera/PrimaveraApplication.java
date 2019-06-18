afd93089cab904ed379530698f9128699f9605a4
genius.choi <csj4032@gmail.com>
2019-06-19 02:30:34 +0900

    chap02

--- chap03/src/main/java/com/genius/primavera/PrimaveraApplication.java
+++ chap03/src/main/java/com/genius/primavera/PrimaveraApplication.java
@@ -2,7 +2,6 @@ package com.genius.primavera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimaveraApplication {
