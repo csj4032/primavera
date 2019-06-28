255d240f9b08b8c803db12e410f725779da68621
genius.choi <csj4032@gmail.com>
2019-06-28 12:25:26 +0900

    chap12

--- chap12/src/main/java/com/genius/primavera/application/storage/FileSystemStorageService.java
+++ chap12/src/main/java/com/genius/primavera/application/storage/FileSystemStorageService.java
@@ -3,11 +3,13 @@ package com.genius.primavera.application.storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Service
 public class FileSystemStorageService implements StorageService {
@@ -20,17 +22,42 @@ public class FileSystemStorageService implements StorageService {
     }
 
     @Override
-    public void deleteAll() {
-        FileSystemUtils.deleteRecursively(rootLocation.toFile());
-    }
-
-    @Override
     public void init() {
         try {
             Files.createDirectories(rootLocation);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new StorageException("Could not initialize storage", e);
         }
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + file.getOriginalFilename());
+            }
+            Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename()));
+        } catch (Exception ioException) {
+            throw new StorageException("Failed to store file " + file.getOriginalFilename());
+        }
+    }
+
+    @Override
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1).filter(path -> !path.equals(this.rootLocation)).map(rootLocation::relativize);
+        } catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
     }
 }
--- chap12/src/main/java/com/genius/primavera/application/storage/StorageService.java
+++ chap12/src/main/java/com/genius/primavera/application/storage/StorageService.java
@@ -1,8 +1,20 @@
 package com.genius.primavera.application.storage;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    Path load(String filename);
+
+    Stream<Path> loadAll();
 
     void deleteAll();
 
-    void init();
 }
