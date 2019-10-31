aee26c5e82df1c119265cc9ffd3fd5fc03671ac4
genius.choi <csj4032@gmail.com>
2019-10-31 17:07:03 +0900

    Hystrix, Openfeign, Turbine

--- hello/src/main/java/com/genius/primavera/HelloApplication.java
+++ hello/src/main/java/com/genius/primavera/HelloApplication.java
@@ -2,8 +2,16 @@ package com.genius.primavera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCaching
+@EnableHystrix
+@EnableFeignClients
 @SpringBootApplication
+@EnableHystrixDashboard
 public class HelloApplication {
 
 	public static void main(String[] args) {
