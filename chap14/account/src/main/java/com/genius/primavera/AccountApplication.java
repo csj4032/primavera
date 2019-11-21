8ec72b934014fbe5acb5f4deb0fd180f4822f72f
genius.choi <csj4032@gmail.com>
2019-11-21 13:35:02 +0900

    Flux flatMap

--- chap14/account/src/main/java/com/genius/primavera/AccountApplication.java
+++ chap14/account/src/main/java/com/genius/primavera/AccountApplication.java
@@ -16,5 +16,4 @@ public class AccountApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
-
 }
--- chap14/account/src/main/java/com/genius/primavera/DataInitializer.java
+++ chap14/account/src/main/java/com/genius/primavera/DataInitializer.java
@@ -2,8 +2,10 @@ package com.genius.primavera;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 import reactor.core.publisher.Flux;
 
 import java.time.LocalDateTime;
@@ -13,18 +15,21 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
-	private final AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
 
-	@Override
-	public void run(String... args) {
-		log.info("start data initialization  ... {}");
-		accountRepository
-				.deleteAll()
-				.thenMany(Flux.just(
-						new User(99l, "Name1", LocalDateTime.now()),
-						new User(100l, "Name2", LocalDateTime.now()),
-						new User(101l, "Name3", LocalDateTime.now())).flatMap(user -> accountRepository.save(user)))
-				.log()
-				.subscribe(null, null, () -> log.info("done initialization..."));
-	}
+    @Override
+    public void run(String... args) {
+        log.info("start data initialization  ... {}");
+        accountRepository
+                .deleteAll()
+                .thenMany(Flux.just(newUserInstance(100)).flatMap(user -> accountRepository.save(user)))
+                .log()
+                .subscribe(null, null, () -> log.info("done initialization..."));
+    }
+
+    private User[] newUserInstance(int n) {
+        User[] users = new User[n];
+        for (int i = 0; i < n; i++) users[i] = new User(i, "Name" + i, LocalDateTime.now());
+        return users;
+    }
 }
