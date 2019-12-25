c05a84f61ad0ba25f2e231f0cfab7e02c15f49c5
genius.choi <csj4032@gmail.com>
2019-12-25 12:39:43 +0900

    switch expression

--- hello/src/main/java/com/genius/primavera/EnumType.java
+++ hello/src/main/java/com/genius/primavera/EnumType.java
@@ -1,7 +1,30 @@
 package com.genius.primavera;
 
+import lombok.Getter;
+
+@Getter
 public enum EnumType {
-	ABC,
-	DEF,
-	GHI
+	ABC("abc"),
+	DEF("def"),
+	GHI("ghi"),
+	XYZ("xyz");
+
+	private String alias;
+
+	EnumType(String alias) {
+		this.alias = alias;
+	}
+
+	public static EnumType of(String alias) {
+		return switch (alias) {
+			case "abc":
+				yield ABC;
+			case "def":
+				yield DEF;
+			case "ghi":
+				yield GHI;
+			default:
+				yield XYZ;
+		};
+	}
 }
