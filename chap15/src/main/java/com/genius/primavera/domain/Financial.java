73f761f572653e3b7260dc193ef122127d856c0f
genius.choi <csj4032@gmail.com>
2019-12-28 20:48:51 +0900

    financial build move

--- chap15/src/main/java/com/genius/primavera/domain/Financial.java
+++ chap15/src/main/java/com/genius/primavera/domain/Financial.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.poi.ss.usermodel.Row;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -26,4 +27,22 @@ public class Financial {
 	private BigDecimal cogs;
 	private BigDecimal profit;
 	private Instant date;
+
+	public static Financial of(Row row) {
+		return Financial.builder()
+				.segment(row.getCell(0).getStringCellValue())
+				.country(row.getCell(1).getStringCellValue())
+				.product(row.getCell(2).getStringCellValue())
+				.discountBand(row.getCell(3).getStringCellValue())
+				.unitsSold(row.getCell(4).getNumericCellValue())
+				.manufacturingPrice(BigDecimal.valueOf(row.getCell(5).getNumericCellValue()))
+				.salePrice(BigDecimal.valueOf(row.getCell(6).getNumericCellValue()))
+				.grossSales(BigDecimal.valueOf(row.getCell(7).getNumericCellValue()))
+				.discounts(row.getCell(8).getStringCellValue())
+				.sales(BigDecimal.valueOf(row.getCell(9).getNumericCellValue()))
+				.cogs(BigDecimal.valueOf(row.getCell(10).getNumericCellValue()))
+				.profit(BigDecimal.valueOf(row.getCell(11).getNumericCellValue()))
+				.date(row.getCell(12).getDateCellValue().toInstant())
+				.build();
+	}
 }
