0b0c86c4cb74de90115c1c91c82186f20906beaa
genius.choi <csj4032@gmail.com>
2019-12-30 00:33:35 +0900

    template

--- chap15/src/main/java/com/genius/primavera/application/factory/ExcelTypeFile.java
+++ chap15/src/main/java/com/genius/primavera/application/factory/ExcelTypeFile.java
@@ -1,17 +1,12 @@
 package com.genius.primavera.application.factory;
 
+import com.genius.primavera.application.template.ExcelImportTemplate;
 import com.genius.primavera.domain.ExcelImportRequest;
 import com.genius.primavera.domain.ExcelImportResponse;
 import com.genius.primavera.domain.Financial;
 import com.genius.primavera.domain.MediaType;
-import com.monitorjbl.xlsx.StreamingReader;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.*;
-import org.springframework.util.StopWatch;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
@@ -22,28 +17,7 @@ public class ExcelTypeFile extends AbstractResponseFactory implements ResponseFa
 	}
 
 	public ExcelImportResponse getExcelImportResponse() {
-		List<Financial> financialList = new ArrayList<>();
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
-		try {
-			Workbook workbook = StreamingReader.builder().open(excelImportRequest.getInputStream());
-			Iterator<Sheet> sheets = workbook.sheetIterator();
-			while (sheets.hasNext()) {
-				Sheet sheet = sheets.next();
-				Iterator<Row> rows = sheet.iterator();
-				while (rows.hasNext()) {
-					Row row = rows.next();
-					if (row.getRowNum() == 0) continue;
-					financialList.add(Financial.of(row));
-				}
-			}
-			workbook.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		stopWatch.stop();
-		log.debug("elapsed : " + stopWatch.getTotalTimeMillis());
-		log.debug("financialList : {}" + financialList);
+		List<Financial> financialList = new ExcelImportTemplate<Financial>(excelImportRequest.getInputStream()).read(Financial::of);
 		return new ExcelImportResponse(excelImportRequest.getName(), excelImportRequest.getSize(), MediaType.EXCEL_TYPE, "row count : " + financialList.size());
 	}
 }
