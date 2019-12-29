9270c1fad2aff959ac1b8d5979cfa66b51238e68
genius.choi <csj4032@gmail.com>
2019-12-30 00:39:16 +0900

    template interface

--- chap15/src/main/java/com/genius/primavera/application/template/ExcelImportTemplate.java
+++ chap15/src/main/java/com/genius/primavera/application/template/ExcelImportTemplate.java
@@ -1,45 +1,12 @@
 package com.genius.primavera.application.template;
 
-import com.monitorjbl.xlsx.StreamingReader;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 
-@Slf4j
-public class ExcelImportTemplate<T> {
+public interface ExcelImportTemplate<T> {
 
-	private final InputStream inputStream;
+	List<T> read(final Function<Row, T> function);
 
-	public ExcelImportTemplate(final InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
-
-	public List<T> read(final Function<Row, T> function) {
-		List<T> list = Collections.emptyList();
-		Workbook workbook = StreamingReader.builder().open(inputStream);
-		Iterator<Sheet> sheets = workbook.sheetIterator();
-		while (sheets.hasNext()) {
-			Sheet sheet = sheets.next();
-			Iterator<Row> rows = sheet.iterator();
-			while (rows.hasNext()) {
-				Row row = rows.next();
-				if (row.getRowNum() == 0) continue;
-				list.add(function.apply(row));
-			}
-		}
-		try {
-			inputStream.close();
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-		return list;
-	}
 }
