cb5f10bb128039aa77fab356821e6a1664c25859
genius.choi <csj4032@gmail.com>
2020-02-22 12:58:37 +0900

    AJAX Javascript Array

--- chap05/src/main/java/com/genius/primavera/interfaces/AjaxController.java
+++ chap05/src/main/java/com/genius/primavera/interfaces/AjaxController.java
@@ -3,10 +3,9 @@ package com.genius.primavera.interfaces;
 import com.genius.primavera.domain.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -45,5 +44,15 @@ public class AjaxController {
 	@PostMapping(value = "/ajax/json/data")
 	public @ResponseBody User postJsonData(@RequestBody User user) {
 		return user;
+	}
+
+	@DeleteMapping(value = "/ajax/json/data")
+	public @ResponseBody User deleteJsonData(@RequestBody User user) {
+		return user;
+	}
+
+	@PostMapping(value = "/ajax/form/array")
+	public @ResponseBody List<Long> getJsonArray(@RequestParam(name = "numbers[]") List<Long> numbers) {
+		return numbers;
 	}
 }
