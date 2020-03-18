edb9acd25a81525f1c1b642f494b901c2aeabecb
genius.choi <genius.choi@wemakeprice.com>
2020-03-19 01:26:33 +0900

    Chap8 PrimaveraPasswordEncoder add

---
+++ chap08/src/main/java/com/genius/primavera/infrastructure/security/PrimaveraPasswordEncoder.java
@@ -0,0 +1,43 @@
+package com.genius.primavera.infrastructure.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+public class PrimaveraPasswordEncoder implements PasswordEncoder {
+
+	private static final String PREFIX = "*";
+
+	private static final PasswordEncoder INSTANCE = new PrimaveraPasswordEncoder();
+
+	public static PasswordEncoder getInstance() {
+		return INSTANCE;
+	}
+
+	private PrimaveraPasswordEncoder() {
+	}
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return digest(rawPassword.toString());
+	}
+
+	private String digest(String rawPassword) {
+		return PREFIX + DigestUtils.sha1Hex(DigestUtils.sha1(rawPassword)).toUpperCase();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		if (encodedPassword == null || encodedPassword.length() == 0) {
+			log.warn("Empty encoded password");
+			return false;
+		}
+		return isEqual(digest(rawPassword.toString()), encodedPassword);
+	}
+
+	private boolean isEqual(String rawPassword, String encodedPassword) {
+		return rawPassword.equals(encodedPassword);
+	}
+}
--- chap08/src/main/java/com/genius/primavera/infrastructure/security/PrimaveraSecurityConfig.java
+++ chap08/src/main/java/com/genius/primavera/infrastructure/security/PrimaveraSecurityConfig.java
@@ -2,6 +2,7 @@ package com.genius.primavera.infrastructure.security;
 
 import com.genius.primavera.infrastructure.filter.PrimaveraFilter;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -21,63 +23,67 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class PrimaveraSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private AuthenticationSuccessHandler successHandler = (request, response, authentication) -> log.info("success : " + request.getContextPath());
-    private AuthenticationFailureHandler failureHandler = (request, response, authentication) -> log.info("failure : " + request.getContextPath());
+	private AuthenticationSuccessHandler successHandler = (request, response, authentication) -> log.info("success : " + request.getContextPath());
+	private AuthenticationFailureHandler failureHandler = (request, response, authentication) -> log.info("failure : " + request.getContextPath());
 
-    @Autowired
-    private PrimaveraUserDetailsService primaveraUserDetailsService;
+	private final PrimaveraUserDetailsService primaveraUserDetailsService;
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser("Genius").password("{bcrypt}$2a$10$7UEHLpn1r4gZY2qxiZFJ5.7wa3Hdz8IXgxUtFogy0Ac10fh7TG4V.").roles("USER")
-                .and()
-                .withUser("Marcus Tullius Cicero").password(encoder.encode("password")).roles("MANAGER")
-                .and()
-                .withUser("Julius Caesar").password(encoder.encode("password")).roles("ADMINISTRATOR");
-        auth.authenticationProvider(authenticationProvider());
-    }
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		DelegatingPasswordEncoder encoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		encoder.setDefaultPasswordEncoderForMatches(PrimaveraPasswordEncoder.getInstance());
+		auth.inMemoryAuthentication()
+				.withUser("Genius").password("{bcrypt}$2a$10$7UEHLpn1r4gZY2qxiZFJ5.7wa3Hdz8IXgxUtFogy0Ac10fh7TG4V.").roles("USER")
+				.and()
+				.withUser("Marcus Tullius Cicero").password(encoder.encode("password")).roles("MANAGER")
+				.and()
+				.withUser("Julius Caesar").password(encoder.encode("password")).roles("ADMINISTRATOR");
+		auth.authenticationProvider(authenticationProvider());
+	}
 
-    @Override
-    public void configure(WebSecurity webSecurity) throws Exception {
-        webSecurity.ignoring().antMatchers(HttpMethod.GET, "/resources/**", "/bower_components/**", "/dist/**", "/plugins/**", "/favicon.ico");
-    }
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+		webSecurity.ignoring().antMatchers(HttpMethod.GET, "/resources/**", "/bower_components/**", "/dist/**", "/plugins/**", "/favicon.ico");
+	}
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterAfter(new PrimaveraFilter(), UsernamePasswordAuthenticationFilter.class)
-                .formLogin()
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .loginPage("/login")
-                .loginProcessingUrl("/signin")
-                .successHandler(successHandler)
-                .defaultSuccessUrl("/index", true)
-                .failureHandler(failureHandler)
-                .failureUrl("/login?error=true")
-                .and()
-                .logout()
-                .logoutUrl("/signout")
-                .deleteCookies("JSESSIONID");
-    }
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/login").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.addFilterAfter(new PrimaveraFilter(), UsernamePasswordAuthenticationFilter.class)
+				.formLogin()
+				.usernameParameter("email")
+				.passwordParameter("password")
+				.loginPage("/login")
+				.loginProcessingUrl("/signin")
+				.successHandler(successHandler)
+				.defaultSuccessUrl("/index", true)
+				.failureHandler(failureHandler)
+				.failureUrl("/login?error=true")
+				.and()
+				.logout()
+				.logoutUrl("/signout")
+				.deleteCookies("JSESSIONID");
+	}
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(primaveraUserDetailsService);
-        return authProvider;
-    }
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(primaveraUserDetailsService);
+		return authProvider;
+	}
 }
