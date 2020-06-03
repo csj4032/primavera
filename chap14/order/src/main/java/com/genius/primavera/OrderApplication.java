91a478107099f146fc80531fc99ed9fe3803d8c6
csj4032 <csj4032@gmail.com>
2020-06-03 12:46:43 +0900

    Order Sever r2dbc add

--- chap14/order/src/main/java/com/genius/primavera/OrderApplication.java
+++ chap14/order/src/main/java/com/genius/primavera/OrderApplication.java
@@ -1,26 +1,37 @@
 package com.genius.primavera;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.web.reactive.function.server.RouterFunction;
-
-import java.util.List;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
+@Slf4j
 @SpringBootConfiguration
 @EnableAutoConfiguration
+@EnableR2dbcRepositories
 public class OrderApplication {
 
 	private static String USERS_USER_ID_ORDER_URL = "users/{userId}/orders";
-	private static ParameterizedTypeReference<List<Order>> ORDERS_TYPE_REF = new ParameterizedTypeReference<>() {
-	};
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(OrderApplication.class).initializers((GenericApplicationContext context) -> context.registerBean(RouterFunction.class, () -> route().GET(USERS_USER_ID_ORDER_URL, request -> ok().body(context.getBean(OrderService.class).findByUserId(request.pathVariable("userId")), ORDERS_TYPE_REF)).build())).build().run(args);
+
+		new SpringApplicationBuilder(OrderApplication.class)
+				.initializers((GenericApplicationContext context) -> {
+					context.registerBean(RouterFunction.class, () -> {
+						var orderRepository = context.getBean(OrderRepository.class);
+						var orderService = new OrderServiceImpl(orderRepository);
+						return route().GET(USERS_USER_ID_ORDER_URL, request -> {
+							return ok().body(orderService.findByUserId(request.pathVariable("userId")), Order.class);
+						}).build();
+					});
+				})
+				.build()
+				.run(args);
 	}
 }
