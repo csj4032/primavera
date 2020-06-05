0288d7b4cea40ffc45eaee723b37a11db37b2e4e
csj4032 <csj4032@gmail.com>
2020-06-05 18:49:15 +0900

    Sonar Lint

--- chap15/src/main/java/com/genius/primavera/application/ExcelFileValid.java
+++ chap15/src/main/java/com/genius/primavera/application/ExcelFileValid.java
@@ -3,6 +3,7 @@ package com.genius.primavera.application;
 import java.io.IOException;
 import java.io.InputStream;
 
+@FunctionalInterface
 public interface ExcelFileValid {
 
 	InputStream getInputStream() throws IOException;
