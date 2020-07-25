d5823f8990a68a7ecb1c4d18858aa1aceb6af69f
genius.choi <csj4032@gmail.com>
2020-07-25 18:19:14 +0900

    Kryo, Snappy

--- chap13/src/main/java/com/genius/primavera/infrastructure/serializer/KryoSerializer.java
+++ chap13/src/main/java/com/genius/primavera/infrastructure/serializer/KryoSerializer.java
@@ -3,7 +3,6 @@ package com.genius.primavera.infrastructure.serializer;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-import com.esotericsoftware.kryo.pool.KryoCallback;
 import com.esotericsoftware.kryo.pool.KryoFactory;
 import com.esotericsoftware.kryo.pool.KryoPool;
 import lombok.extern.slf4j.Slf4j;
@@ -14,23 +13,17 @@ import java.io.ByteArrayOutputStream;
 @Slf4j
 public class KryoSerializer<T> {
 
-	private static final KryoFactory factory = new KryoFactory() {
-
-		public Kryo create() {
-			Kryo kryo = new Kryo();
-			try {
-
-				kryo.setRegistrationRequired(false);
-				//NPE bug fix (Collection  deserialization)
-				((Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy())
-						.setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
-			} catch (Exception e) {
-				log.error(e.getMessage(), e);
-			}
-			return kryo;
+	private static final KryoFactory factory = () -> {
+		Kryo kryo = new Kryo();
+		try {
+			kryo.setRegistrationRequired(false);
+			//NPE bug fix (Collection  deserialization)
+			((Kryo.DefaultInstantiatorStrategy) kryo.getInstantiatorStrategy()).setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		}
+		return kryo;
 	};
-
 
 	private static final KryoPool pool = new KryoPool.Builder(factory).softReferences().build();
 
@@ -39,18 +32,12 @@ public class KryoSerializer<T> {
 	 * @return
 	 */
 	public static byte[] serialize(final Object obj) {
-
-		return pool.run(new KryoCallback<byte[]>() {
-
-			@Override
-			public byte[] execute(Kryo kryo) {
-				ByteArrayOutputStream stream = new ByteArrayOutputStream();
-				Output output = new Output(stream);
-				kryo.writeClassAndObject(output, obj);
-				output.close();
-				return stream.toByteArray();
-			}
-
+		return pool.run(kryo -> {
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			Output output = new Output(stream);
+			kryo.writeClassAndObject(output, obj);
+			output.close();
+			return stream.toByteArray();
 		});
 	}
 
@@ -60,15 +47,9 @@ public class KryoSerializer<T> {
 	 * @return
 	 */
 	public static <V> V deserialize(final byte[] objectData) {
-
-		return (V) pool.run(new KryoCallback<V>() {
-
-			@Override
-			public V execute(Kryo kryo) {
-				Input input = new Input(objectData);
-				return (V) kryo.readClassAndObject(input);
-			}
-
+		return (V) pool.run(kryo -> {
+			Input input = new Input(objectData);
+			return (V) kryo.readClassAndObject(input);
 		});
 	}
 
@@ -80,14 +61,6 @@ public class KryoSerializer<T> {
 	 * @return
 	 */
 	public static <V> V deepCopy(final V obj) {
-
-		return (V) pool.run(new KryoCallback<V>() {
-			@Override
-			public V execute(Kryo kryo) {
-				return (V) kryo.copy(obj);
-			}
-
-		});
+		return (V) pool.run(kryo -> (V) kryo.copy(obj));
 	}
-
 }
