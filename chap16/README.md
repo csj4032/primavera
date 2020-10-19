b88842fd7bc6a1152eb9c52609c31b16acc96426
genius.choi <csj4032@gmail.com>
2020-10-20 02:07:53 +0900

    Batch Example

---
+++ chap16/README.md
@@ -0,0 +1,2 @@
+## chap16
+### SQL (MYSQL) : https://github.com/spring-projects/spring-batch/blob/master/spring-batch-core/src/main/resources/org/springframework/batch/core/schema-mysql.sql
---
+++ chap16/src/main/java/com/genius/primavera/domain/Person.java
@@ -0,0 +1,13 @@
+package com.genius.primavera.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+	public String firstName;
+	public String lastName;
+}
