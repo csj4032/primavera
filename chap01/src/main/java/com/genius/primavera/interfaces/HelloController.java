ef43a140d15dade8d43d11b338149804d30e8c3b
genius.choi <csj4032@gmail.com>
2020-10-27 00:35:37 +0900

    bean scope

--- chap01/src/main/java/com/genius/primavera/interfaces/HelloController.java
+++ chap01/src/main/java/com/genius/primavera/interfaces/HelloController.java
@@ -1,6 +1,6 @@
 package com.genius.primavera.interfaces;
 
-import com.genius.primavera.application.IHelloService;
+import com.genius.primavera.application.ScopeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,37 +9,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/")
 public class HelloController {
 
-	private final IHelloService helloService;
-	private final IHelloService helloServiceRequest;
-	private final IHelloService helloServicePrototype;
+	private final ScopeService scopeService;
+	private final ScopeService scopeServiceRequest;
+	private final ScopeService scopeServiceProtoType;
+	private final ScopeService scopeServiceRequestAnonymous;
 
-	@Autowired
-	@Qualifier("helloServicePrototype")
-	private IHelloService helloServicePrototype2;
+	@Resource(name = "scopeServiceProtoType")
+	private ScopeService scopeServiceProtoType2;
 
-	@Autowired
-	@Qualifier("helloService")
-	private IHelloService helloService2;
+	@Resource(name = "scopeServiceRequest")
+	private ScopeService scopeServiceRequest2;
+
+	@Resource(name = "scopeService")
+	private ScopeService scopeService2;
 
 	@GetMapping
 	public String helloWorld() {
-		helloService.getUsers();
 		return "Hello World";
 	}
 
 	@GetMapping("/scope")
 	public String scope() {
-		log.info("helloService : {}", helloService);
-		log.info("helloService2 : {}", helloService2);
-		log.info("helloServiceProtoType : {}", helloServicePrototype);
-		log.info("helloServicePrototype2 : {}", helloServicePrototype2);
-		log.info("helloServiceRequest : {}", helloServiceRequest);
+		log.info("scopeService1 : {}", scopeService);
+		log.info("scopeService2 : {}", scopeService2);
+		log.info("scopeServiceProtoType1 : {}", scopeServiceProtoType);
+		log.info("scopeServiceProtoType2 : {}", scopeServiceProtoType2);
+		log.info("scopeServiceRequest1 : {}", scopeServiceRequest);
+		log.info("scopeServiceRequest2 : {}", scopeServiceRequest2);
+		log.info("scopeServiceRequestAnonymous : {}", scopeServiceRequestAnonymous);
 		return "Hello World";
 	}
 }
