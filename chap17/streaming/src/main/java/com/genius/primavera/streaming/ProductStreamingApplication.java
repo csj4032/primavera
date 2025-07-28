fec8cfd9f6ba3dd47b3308da3e57bad72f8cc28d
genius.choi <csj4032@gmail.com>
2025-07-28 10:20:10 +0900

    feat: update build configuration for Spring Boot and Debezium, add Kafka Connect API support

--- chap17/streaming/src/main/java/com/genius/primavera/streaming/ProductStreamingApplication.java
+++ chap17/streaming/src/main/java/com/genius/primavera/streaming/ProductStreamingApplication.java
@@ -3,10 +3,7 @@ package com.genius.primavera.streaming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.kafka.annotation.EnableKafka;
-
 @SpringBootApplication
-@EnableKafka
 @ConfigurationPropertiesScan
 public class ProductStreamingApplication {
 
