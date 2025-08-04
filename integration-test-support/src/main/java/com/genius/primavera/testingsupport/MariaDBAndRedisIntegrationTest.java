106192c744166e29ff4d177115cfe003a6f46d1e
genius.choi <csj4032@gmail.com>
2025-08-05 01:01:58 +0900

    feat: refactor integration tests to use mixin pattern and modernize DTOs with Java records

--- integration-test-support/src/main/java/com/genius/primavera/testingsupport/MariaDBAndRedisIntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package com.genius.primavera.testingsupport;
-
-public interface MariaDBAndRedisIntegrationTest extends MariaDBIntegrationTest, RedisIntegrationTest {
-
-    static void mariadbAndRedisStart() {
-        mariadb.start();
-        redis.start();
-    }
-}
